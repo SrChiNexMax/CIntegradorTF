c73564bf73b85f39243cd34410241cc06772e4e4
Furanshisuko17 <darkfran555@gmail.com>
2022-10-12 15:59:41 -0500

    Arreglados los bugs hechos por Nanami

--- src/main/java/com/utp/integrador/IntegradorFinal/IntegradorFinalApplication.java
+++ src/main/java/com/utp/integrador/IntegradorFinal/IntegradorFinalApplication.java
@@ -2,8 +2,10 @@ package com.utp.integrador.IntegradorFinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:config/mysql/db.properties")
 public class IntegradorFinalApplication {
 
 	public static void main(String[] args) {
