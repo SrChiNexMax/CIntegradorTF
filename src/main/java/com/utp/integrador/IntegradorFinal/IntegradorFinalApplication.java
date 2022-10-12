1a9de1a0e6fdf4f3cb3bbf7d6e1412c8764c69c8
Furanshisuko17 <darkfran555@gmail.com>
2022-10-12 13:58:33 -0500

    Added database connection

--- src/main/java/com/utp/integrador/IntegradorFinal/IntegradorFinalApplication.java
+++ src/main/java/com/utp/integrador/IntegradorFinal/IntegradorFinalApplication.java
@@ -2,8 +2,12 @@ package com.utp.integrador.IntegradorFinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:config/mysql/db.properties")
 public class IntegradorFinalApplication {
 
 	public static void main(String[] args) {
