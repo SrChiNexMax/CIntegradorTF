a3e06f2ba4d9dfae5976960523dff77d70f73627
Furanshisuko17 <darkfran555@gmail.com>
2022-10-14 00:30:17 -0500

    Arreglado error con AlmacenDao

--- src/main/java/com/utp/integrador/IntegradorFinal/models/entity/Almacen.java
+++ src/main/java/com/utp/integrador/IntegradorFinal/models/entity/Almacen.java
@@ -2,12 +2,15 @@ package com.utp.integrador.IntegradorFinal.models.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -15,16 +18,19 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "almacen")
-public class Almacen implements Serializable {
+@PrimaryKeyJoinColumn(name = "idProducto")
+public class Almacen extends Producto implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	
+	/*
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@OneToOne
-	@JoinColumn(nullable = false, name = "idProducto", referencedColumnName = "idProducto")
-	private Producto Producto;
+	private Long idProducto;
 	
+	@OneToOne
+	@PrimaryKeyJoinColumn(name = "idProducto")
+	private Producto Producto;
+	*/
 	private int unidadesExistentes;
 	private int unidadesEnPedido;
 	
--- src/main/java/com/utp/integrador/IntegradorFinal/models/entity/Producto.java
+++ src/main/java/com/utp/integrador/IntegradorFinal/models/entity/Producto.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -17,6 +19,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "producto")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Producto implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
