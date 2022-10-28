dde8bfb9075860f7928aa0156239c3231cdc8cdf
SrChiNexMax <aramansalazar280703@gmail.com>
2022-10-28 00:32:15 -0500

    Implementacion de Apache POI SECCION USUARIOS
    Metan productos y lo demas para hacer ventas y sus reportes :vvvvvvvvvv

---
+++ src/main/java/com/utp/integrador/IntegradorFinal/util/ListarUsuariosExcel.java
@@ -0,0 +1,20 @@
+package com.utp.integrador.IntegradorFinal.util;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractXlsxView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Component("secciones/usuarios.xlsx")
+public class ListarUsuariosExcel extends AbstractXlsxView {
+    @Override
+    protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setHeader("Content-Disposition", "attachment; filename=\"listado-usuarios.xlsx\"");
+        Sheet hoja = workbook.createSheet("Usuarios");
+
+    }
+}
