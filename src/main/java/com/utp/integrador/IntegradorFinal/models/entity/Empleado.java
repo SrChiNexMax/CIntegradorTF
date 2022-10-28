25b6148892fe9de6024c08713dfb61cc134de761
Furanshisuko17 <darkfran555@gmail.com>
2022-10-27 19:12:45 -0500

    Arreglado relacion entre empleado y usuario

--- src/main/java/com/utp/integrador/IntegradorFinal/models/entity/Empleado.java
+++ src/main/java/com/utp/integrador/IntegradorFinal/models/entity/Empleado.java
@@ -3,6 +3,7 @@ package com.utp.integrador.IntegradorFinal.models.entity;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -25,15 +28,18 @@ public class Empleado implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idEmpleado;
 	
-	@ManyToOne
-	@JoinColumn(nullable = false, name = "idUsuario", referencedColumnName = "idUsuario")
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "idUsuario")
+	@MapsId
 	private Usuario usuario;
 	
 	@Column(length = 150)
 	private String nombre;
 	
+	@Column(nullable = true)
 	private int dni;
 	
+	@Column(nullable = true)
 	private int telefono;
 	
 	private Timestamp fechaNacimiento;
--- src/main/java/com/utp/integrador/IntegradorFinal/models/entity/Usuario.java
+++ src/main/java/com/utp/integrador/IntegradorFinal/models/entity/Usuario.java
@@ -4,9 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -19,8 +18,10 @@ public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idUsuario;
+	
+	@OneToOne(mappedBy = "usuario")
+	private Empleado empleado;
 	
 	@Column(length = 100)
 	private String nombre;
