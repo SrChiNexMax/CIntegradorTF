fdef0021d85145704eb7aacfb82bd8af408d7dcc
SrChiNexMax <aramansalazar280703@gmail.com>
2022-10-28 18:57:26 -0500

    Generar Reporte de usuarios completo

--- src/main/java/com/utp/integrador/IntegradorFinal/util/ListarUsuariosExcel.java
+++ src/main/java/com/utp/integrador/IntegradorFinal/util/ListarUsuariosExcel.java
@@ -1,12 +1,16 @@
 package com.utp.integrador.IntegradorFinal.util;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractXlsxView;
+import com.utp.integrador.IntegradorFinal.models.entity.Empleado;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 import java.util.Map;
 
 @Component("secciones/usuarios.xlsx")
@@ -16,5 +20,28 @@ public class ListarUsuariosExcel extends AbstractXlsxView {
         response.setHeader("Content-Disposition", "attachment; filename=\"listado-usuarios.xlsx\"");
         Sheet hoja = workbook.createSheet("Usuarios");
 
+        Row filaTitulo = hoja.createRow(0);
+        Cell celda = filaTitulo.createCell(0);
+        celda.setCellValue("Listado de Usuarios");
+
+        Row filaData = hoja.createRow(2);
+        String[] columnas = {"ID","Nombre","Acceso"};
+        for(int i=0;i<columnas.length;i++){
+            celda = filaData.createCell(i);
+            celda.setCellValue(columnas[i]);
+        }
+
+        List<Empleado> listaU = (List<Empleado>) model.get("empleados");
+
+        int numFila = 3;
+        for (Empleado empleado : listaU){
+            filaData = hoja.createRow(numFila);
+
+            filaData.createCell(0).setCellValue(empleado.getIdEmpleado());
+            filaData.createCell(1).setCellValue(empleado.getNombre());
+            filaData.createCell(2).setCellValue(empleado.getAcceso());
+
+            numFila++;
+        }
     }
 }
