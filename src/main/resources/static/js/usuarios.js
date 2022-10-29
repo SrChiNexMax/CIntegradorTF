056ee5d7f11544f7b614b9f607827c189662f57e
Furanshisuko17 <darkfran555@gmail.com>
2022-10-28 19:23:24 -0500

    Funcionalidad de usuarios

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -13,7 +13,29 @@
 	codigoInput.value = codigo
 	nombreInput.value = nombre
 	accesoSelect.value = acceso
+	var addb = document.getElementById('agregar')
+	addb.setAttribute('disabled', '')
+	var button = document.getElementById(rowId.replace("row", "button"))
+	button.setAttribute("onClick", "removeData('"+rowId+"')")
+	button.innerHTML = "Deseleccionar"
+	
 	console.log(codigo)
 	console.log(nombre)
 	console.log(acceso)	
+}
+
+function removeData(id) {
+	var codigoInput = document.getElementById("idEmpleado")
+	var nombreInput = document.getElementById("nombre")
+	var accesoSelect = document.getElementById("acceso")
+	codigoInput.value = ""
+	nombreInput.value = ""
+	accesoSelect.value = ""
+	var addb = document.getElementById('agregar')
+	addb.removeAttribute('disabled')
+	var button = document.getElementById(id.replace("row", "button"))
+	
+	console.log(id)
+	button.setAttribute("onClick", "getData('" + id + "')")
+	button.innerHTML = "Seleccionar"
 }
