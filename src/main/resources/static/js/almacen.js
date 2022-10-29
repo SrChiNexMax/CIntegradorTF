d8da9dccffc6c754a062a739f1fc1b5ed493fae6
6syan9 <melgar10barca@gmail.com>
2022-10-28 19:44:30 -0500

    Avance de seccion de almacen

--- src/main/resources/static/js/almacen.js
+++ src/main/resources/static/js/almacen.js
@@ -1,5 +1,5 @@
-function getDataMarca(rowId) {
-	var tabla= document.getElementById(rowId).getElementsByTagName("td")
+function getDataMarca(mId) {
+	var tabla= document.getElementById(mId).getElementsByTagName("td")
 	var codigo = tabla[0].innerHTML
 	var nombreMarca = tabla[1].innerHTML
 	var codigoInput = document.getElementById("idMarca")
@@ -10,8 +10,8 @@ function getDataMarca(rowId) {
 	console.log(nombreMarca)
 }
 
-function getDataCategoria(rowId) {
-	var tabla= document.getElementById(rowId).getElementsByTagName("td")
+function getDataCategoria(cId) {
+	var tabla= document.getElementById(cId).getElementsByTagName("td")
 	var codigo = tabla[0].innerHTML
 	var nombreCategoria = tabla[1].innerHTML
 	var codigoInput = document.getElementById("idCategoria")
@@ -23,8 +23,8 @@ function getDataCategoria(rowId) {
 	
 }
 
-function getDataProducto(rowId) {
-	var tabla= document.getElementById(rowId).getElementsByTagName("td")
+function getDataProducto(pId) {
+	var tabla= document.getElementById(pId).getElementsByTagName("td")
 	var codigo = tabla[0].innerHTML
 	var nombreProducto = tabla[1].innerHTML
 	var precioUnidad = tabla[2].innerHTML
@@ -36,18 +36,18 @@ function getDataProducto(rowId) {
 	var codigoInput = document.getElementById("idProducto")
 	var nombreProductoInput = document.getElementById("nombreProducto")
 	var precioUnidadInput = document.getElementById("precioUnidad")
-    var marcaInput = document.getElementById("nombreMarca")
-    var categoriaInput = document.getElementById("nombreCategoria")
+    var marcaInput = document.getElementById("marca")
+    var categoriaInput = document.getElementById("categoria")
     var unidadesExistentesInput = document.getElementById("unidadesExistentes")
     var estaSuspendidoInput = document.getElementById("estaSuspendido")
 
-	codigoInput.value = idProducto
+	codigoInput.value = codigo
 	nombreProductoInput.value = nombreProducto
 	precioUnidadInput.value = precioUnidad
-    marcaInput.value = marca.nombreMarca
-    categoriaInput.value = categoria.nombreCategoria
-    unidadesExistentesInput.value = almacen.unidadesExistentes
-    estaSuspendidoInput.value = almacen.estaSuspendido
+    marcaInput.value = marca
+    categoriaInput.value = categoria
+    unidadesExistentesInput.value = unidadesExistentes
+    estaSuspendidoInput.value = estaSuspendido
 
 	console.log(codigo)
 	console.log(nombreProducto)
