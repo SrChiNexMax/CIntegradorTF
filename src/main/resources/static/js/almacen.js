1717237cbb4d1012bf6fc5eabf87fc08d90d9b29
SrChiNexMax <aramansalazar280703@gmail.com>
2022-11-11 18:20:43 -0500

    Implementación de deseleccionar en clientes, almacen, marcas y categorias
    En producto ver como obtener el dato del dropdown

--- src/main/resources/static/js/almacen.js
+++ src/main/resources/static/js/almacen.js
@@ -1,27 +1,83 @@
+
 function getDataMarca(mId) {
 	var tabla= document.getElementById(mId).getElementsByTagName("td")
 	var codigo = tabla[0].innerHTML
 	var nombreMarca = tabla[1].innerHTML
+
 	var codigoInput = document.getElementById("idMarca")
 	var nombreMarcaInput = document.getElementById("nombreMarca")
+
 	codigoInput.value = codigo
 	nombreMarcaInput.value = nombreMarca
+
+	var addbm = document.getElementById('addMarcaB')
+    addbm.setAttribute('disabled', '')
+    var buttonm = document.getElementById(mId.replace("m", "buttonM"))
+    buttonm.setAttribute("onClick", "removeDataM('"+mId+"')")
+    buttonm.innerHTML = "Deseleccionar"
+
 	console.log(codigo)
 	console.log(nombreMarca)
 }
+
+function removeDataM(id) {
+	var codigoInput = document.getElementById("idMarca")
+	var nombreMarcaInput = document.getElementById("nombreMarca")
+
+	codigoInput.value = ""
+	nombreMarcaInput.value = ""
+
+	var addbM = document.getElementById('addMarcaB')
+	addbM.removeAttribute('disabled')
+	var buttonm = document.getElementById(id.replace("m", "buttonM"))
+
+	console.log(id)
+
+	buttonm.setAttribute("onClick", "getDataMarca('" + id + "')")
+	buttonm.innerHTML = "Seleccionar"
+}
+
+
 
 function getDataCategoria(cId) {
 	var tabla= document.getElementById(cId).getElementsByTagName("td")
 	var codigo = tabla[0].innerHTML
 	var nombreCategoria = tabla[1].innerHTML
+
 	var codigoInput = document.getElementById("idCategoria")
 	var nombreCategoriaInput = document.getElementById("nombreCategoria")
+
 	codigoInput.value = codigo
 	nombreCategoriaInput.value = nombreCategoria
+
+	var addbc = document.getElementById('addCategoriaB')
+    addbc.setAttribute('disabled', '')
+    var buttonc = document.getElementById(cId.replace("c", "buttonC"))
+    buttonc.setAttribute("onClick", "removeDataC('"+cId+"')")
+    buttonc.innerHTML = "Deseleccionar"
+
 	console.log(codigo)
 	console.log(nombreCategoria)
-	
 }
+
+function removeDataC(id) {
+	var codigoInput = document.getElementById("idCategoria")
+	var nombreCategoriaInput = document.getElementById("nombreCategoria")
+
+	codigoInput.value = ""
+	nombreCategoriaInput.value = ""
+
+	var addbC = document.getElementById('addCategoriaB')
+	addbC.removeAttribute('disabled')
+	var buttonc = document.getElementById(id.replace("c", "buttonC"))
+
+	console.log(id)
+
+	buttonc.setAttribute("onClick", "getDataCategoria('" + id + "')")
+	buttonc.innerHTML = "Seleccionar"
+}
+
+
 
 function getDataProducto(pId) {
 	var tabla= document.getElementById(pId).getElementsByTagName("td")
@@ -49,6 +105,12 @@ function getDataProducto(pId) {
     unidadesExistentesInput.value = unidadesExistentes
     estaSuspendidoInput.value = estaSuspendido
 
+    var addbp = document.getElementById('addProductoB')
+    addbp.setAttribute('disabled', '')
+    var buttonp = document.getElementById(pId.replace("p", "buttonP"))
+    buttonp.setAttribute("onClick", "removeDataP('"+pId+"')")
+    buttonp.innerHTML = "Deseleccionar"
+
 	console.log(codigo)
 	console.log(nombreProducto)
 	console.log(precioUnidad)
@@ -56,6 +118,36 @@ function getDataProducto(pId) {
 	console.log(categoria)
     console.log(unidadesExistentes)
     console.log(estaSuspendido)
+}
+
+function encontrarM(marca){
+
+}
+
+function removeDataP(id) {
+	var codigoInput = document.getElementById("idProducto")
+    var nombreProductoInput = document.getElementById("nombreProducto")
+    var precioUnidadInput = document.getElementById("precioUnidad")
+    var marcaInput = document.getElementById("marca")
+    var categoriaInput = document.getElementById("categoria")
+    var unidadesExistentesInput = document.getElementById("unidadesExistentes")
+    var estaSuspendidoInput = document.getElementById("estaSuspendido")
+
+	codigoInput.value = ""
+    nombreProductoInput.value = ""
+    precioUnidadInput.value = ""
+    marcaInput.value = ""
+    categoriaInput.value = ""
+    unidadesExistentesInput.value = ""
+    estaSuspendidoInput.value = ""
+
+	var addbP = document.getElementById('addProductoB')
+	addbP.removeAttribute('disabled')
+	var buttonp = document.getElementById(id.replace("p", "buttonP"))
+
+	console.log(id)
+	buttonp.setAttribute("onClick", "getDataProducto('" + id + "')")
+	buttonp.innerHTML = "Seleccionar"
 }
 
 function limpiarCampos(){
--- src/main/resources/static/js/clientes.js
+++ src/main/resources/static/js/clientes.js
@@ -9,22 +9,55 @@
 	var nombre = tabla[2].innerHTML
 	var telefono = tabla[3].innerHTML
 	var direccion = tabla[4].innerHTML
+
 	var codigoInput = document.getElementById("idCliente")
     var dniRucInput = document.getElementById("dniRuc")
 	var nombreInput = document.getElementById("nombre")
 	var telefonoInput = document.getElementById("telefono")
 	var direccionInput = document.getElementById("direccion")
+
 	codigoInput.value = codigo
     dniRucInput.value = dniRuc
 	nombreInput.value = nombre
 	telefonoInput.value = telefono
 	direccionInput.value = direccion
+
+	var addb = document.getElementById('addClienteB')
+    addb.setAttribute('disabled', '')
+    var button = document.getElementById(rowId.replace("row", "button"))
+    button.setAttribute("onClick", "removeData('"+rowId+"')")
+    button.innerHTML = "Deseleccionar"
+
 	console.log(codigo)
     console.log(dniRuc)
 	console.log(nombre)
 	console.log(telefono)
 	console.log(direccion)
 	
+}
+
+function removeData(id) {
+
+	var codigoInput = document.getElementById("idCliente")
+    var dniRucInput = document.getElementById("dniRuc")
+	var nombreInput = document.getElementById("nombre")
+	var telefonoInput = document.getElementById("telefono")
+	var direccionInput = document.getElementById("direccion")
+
+	codigoInput.value = ""
+    dniRucInput.value = ""
+    nombreInput.value = ""
+    telefonoInput.value = ""
+    direccionInput.value = ""
+
+	var addb = document.getElementById('addClienteB')
+	addb.removeAttribute('disabled')
+	var button = document.getElementById(id.replace("row", "button"))
+
+	console.log(id)
+
+	button.setAttribute("onClick", "getDataMarca('" + id + "')")
+	button.innerHTML = "Seleccionar"
 }
 
 function limpiarCampos(){
