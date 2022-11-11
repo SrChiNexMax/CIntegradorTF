08770d078c064d40812daf53f7beef1b47486f40
6syan9 <melgar10barca@gmail.com>
2022-11-11 11:37:54 -0500

    Arreglo 2

--- src/main/resources/static/js/nuevaVentas.js
+++ src/main/resources/static/js/nuevaVentas.js
@@ -1,8 +1,13 @@
 function obtenerDato() {
 	var codigo= document.getElementById('idPro')
-    codigo.addEventListener('click',(event) =>{
+    codigo.addEventListener('keyup',(event) =>{
 
         console.log(event);
+
+        var inputCodigo= event.path[0].value;
+        
+        var codigoInput = document.getElementById('idProducto')
+	    codigoInput.value = inputCodigo
     });
     
 }
