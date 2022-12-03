234dacafe85d79a1ca761a8a522e3d543c9d7e80
Furanshisuko17 <darkfran555@gmail.com>
2022-12-02 20:55:14 -0500

    Funcionalidad desfacturar implementada en caja

--- src/main/java/com/utp/integrador/IntegradorFinal/controllers/CajaController.java
+++ src/main/java/com/utp/integrador/IntegradorFinal/controllers/CajaController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.utp.integrador.IntegradorFinal.models.dao.VentasDao;
 import com.utp.integrador.IntegradorFinal.models.entity.Ventas;
@@ -24,6 +26,15 @@ public class CajaController {
 		model.addAttribute("ventas", ventas);
 		
 		return "secciones/caja";
+	}
+	
+	@GetMapping("/desfacturar/{id}")
+	public String desfacturarVenta(@PathVariable(name="id") Long id) {
+		Ventas venta = ventaService.encontrarVentaPorId(id);
+		venta.setFacturado(false);
+		ventaService.guardar(venta);
+		
+		return "redirect:/caja";
 	}
 	
 	
