2632b9e8c4cadc54db1be41c20e95dcadd906782
Furanshisuko17 <darkfran555@gmail.com>
2022-12-16 13:16:40 -0500

    Update SecurityConfiguration.java

--- src/main/java/com/utp/integrador/IntegradorFinal/config/SecurityConfiguration.java
+++ src/main/java/com/utp/integrador/IntegradorFinal/config/SecurityConfiguration.java
@@ -48,29 +48,5 @@ public class SecurityConfiguration {
 		
         return http.build();
     }
-	
-/*
- * @Override protected void configure(AuthenticationManagerBuilder auth) throws
- * Exception { auth.userDetailsService(userDetailsService); }
- */
-	
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests()
-//				.antMatchers("/editar/**", "/agregar/**", "/eliminar")
-//			        .hasRole("ADMIN") 
-//			    .antMatchers("/")
-//			        .hasAnyRole("USUARIO","ADMIN")
-//			    .and()
-//			        .formLogin()
-//			        .loginPage("/login")
-//			    .and()
-//			        .exceptionHandling().accessDeniedPage("/errores/403") ;
-//		
-// 		http.authorizeRequests()
-//		.antMatchers().hasAnyRole("ADMIN","USUARIO")
-//		.antMatchers("/", "/**").permitAll().and().formLogin().loginPage("/login");
-//	}
-	
 
 }
